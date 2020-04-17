5739daefffc0f81b838b0a407ee2ab0ddca8d96d
Jessica <56430939+jessica440@users.noreply.github.com>
2020-04-17 16:27:29 +0100

    Added in drop down text and fixed carousel
    The 'prev' button works and shows the images on a loop instead of stopping on the first image

--- carousel2.js
+++ carousel2.js
@@ -2,6 +2,7 @@ var image = document.querySelectorAll('.slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,1800);
 
+
 function nextSlide() {
 	image[currentSlide].className = 'slide';
 	currentSlide = (currentSlide + 1) % image.length;
@@ -9,7 +10,9 @@ function nextSlide() {
 }
 
 function prevSlide() {
+	// var newmg = document.getElementById('img').innerHTML = "<img src='slide3.png'>"
 	if (currentSlide < 1) {
+		currentSlide = (image.length - 1) % image.length;
 		return image[image.length - 1];
 	} else {
 	image[currentSlide].className = 'slide';
@@ -39,6 +42,43 @@ pauseButton.onclick = function() {
 	}
 };
 
-// document.addEventListener(onkeydown, function nextSlide(){
-// 	case 39;;
-//   });
+// var input = document.getElementById("myInput");
+// input.addEventListener("keyup", function(event) {
+//   if (event.keyCode === 37) {
+// 	event.prevSlide();
+//   } else (event.keyCode === 39) {
+// 	  event.nextSlide();
+//   }
+// });
+
+// document.onkeydown = function arrowNav(e) {
+// 	switch (e.keydown) {
+// 		if (e === case "37") {
+// 			prevSlide();
+// 	} else (e === case "39") {
+// 		return nextSlide();
+// 	}
+// }
+
+
+// document.onkeydown = function arrowNav(n) {
+// 	if (n == 37) {
+// 		return function prevSlide()}
+// 	else (n == 39) {
+// 		return function nextSlide()}
+// 	}
+
+var text = document.getElementsByClassName("me");
+var i;
+
+for (i = 0; i < text.length; i++) {
+  text[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
